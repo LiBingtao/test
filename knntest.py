bf3692bf8480b895254729acccf5d085ed477187
BingtaoLi <67784273@qq.com>
2017-04-12 20:33:50 +0800

    sklearn learn

--- knntest.py
+++ knntest.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 
 train_data = np.loadtxt('C:/Users/Bingtao LI/PycharmProjects/text/train_data.csv', delimiter=",", skiprows=0)
-train_target = np.loadtxt('C:/Users/Bingtao LI/PycharmProjects/text/train_target.csv', delimiter=",", skiprows=0)
+train_target = np.loadtxt('C:/Users/Bingtao LI/PycharmProjects/text/train_target3.csv', delimiter=",", skiprows=0)
 train_target.reshape(1,-1)
 test_data= np.loadtxt('C:/Users/Bingtao LI/PycharmProjects/text/test_data.csv', delimiter=",", skiprows=0)
-test_target= np.loadtxt('C:/Users/Bingtao LI/PycharmProjects/text/test_target.csv', delimiter=",", skiprows=0)
+test_target= np.loadtxt('C:/Users/Bingtao LI/PycharmProjects/text/test_target3.csv', delimiter=",", skiprows=0)
 test_target.reshape(1,-1)
 
 knn = neighbors.KNeighborsClassifier(5)
